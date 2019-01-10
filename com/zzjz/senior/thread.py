b044a3a879058c5befe8603d373a7b62ad03c1bc
zhouyswo <zhouyswo@163.com>
2019-01-10 09:06:13 +0800

    提交线程相关demo

--- com/zzjz/senior/thread.py
+++ com/zzjz/senior/thread.py
@@ -14,4 +14,52 @@
 # python代码的执行是由python虚拟机进行控制
 # 在主循环中只能有一个控制线程在执行
 
+import time
+# import _thread  as td
+# def fc1():
+#     time.sleep(3)
+#     print("i am walking")
+# def fc2():
+#     time.sleep(2)
+#     print("i am walking to")
+# def ff():
+#     td.start_new_thread(fc1, ())
+#     td.start_new_thread(fc2, ())
+#     time.sleep(7)
+#     print("finish")
+# ff()
 
+# threading
+# 直接利用threading.Thread 生成Thread示例
+# threading.Thread(target="",args=(''''''))创建一个线程对象，再使用start方法启动
+# 守护线程 daemon
+#
+import threading
+#
+# def fc1():
+#     time.sleep(3)
+#     print("i am walking")
+# def fc2():
+#     time.sleep(2)
+#     print("i am walking to")
+# def ff():
+#     # td.start_new_thread(fc1, ())
+#     t1=threading.Thread(target=fc1,args=())
+#     # td.start_new_thread(fc2, ())
+#     t2 =threading.Thread(target=fc2, args=())
+#     t1.start()
+#     t2.start()
+#     time.sleep(7)
+#     print("finish")
+# ff()
+
+
+# 可以继承自threading.Thread创建一个线程
+# 需要重写run方法
+
+# class NyThread(threading.Thread):
+#     def run(self):
+#         print("i am custom thread")
+#
+# t = NyThread()
+# t.start()
